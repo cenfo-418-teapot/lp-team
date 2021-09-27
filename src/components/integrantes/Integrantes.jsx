788f804432a9594759299117dd9e0cee612227aa
Sebastian Soto <68821833+Sebastian-Soto-M@users.noreply.github.com>
2021-09-26 19:09:38 -0600

    feat(integrantes): Card con nombre y rol del integrante (#2)

--- src/components/integrantes/Integrantes.jsx
+++ src/components/integrantes/Integrantes.jsx
@@ -1,31 +1,54 @@
-import Container from '@mui/material/Container'
-import Grid from '@mui/material/Grid'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
-function Integrantes() {
-  return (    
-    <Container maxWidth="100%" sx={{height: '100%'}}>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        sx={{height: '100%'}}
-      >
-        <Grid item>
-          <Box sx={{height: '100%'}}>
-            <Typography variant="h6" component="h1">
-              Integrantes....Un texto de ejemplo...
-            </Typography>
-            <Typography variant="h6" component="h1">
-              Un texto de ejemplo...
-            </Typography>
-          </Box>
-        </Grid>  
-      </Grid>
-    </Container>
-  )
+function Item(props) {
+  return (
+    <Grid item>
+      <Card sx={{ maxWidth: 345 }} elevation={4}>
+        <CardMedia
+          component="img"
+          src={process.env.PUBLIC_URL + props.src}
+          alt={props.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" align="center">
+            {props.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" align="center">
+            {props.role}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
 }
 
-export default Integrantes
+function Integrantes() {
+  return (
+    <Container maxWidth="100%" sx={{ height: "100%" }}>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        spacing={10}
+        alignItems="center"
+        sx={{ height: "100%" }}
+      >
+        <Item name="Josué Brenes" src="josue.svg" role="Calidad"></Item>
+        <Item name="Isaac Miranda" src="isaac.svg" role="Desarrollo"></Item>
+        <Item name="Mauricio Poveda" src="mauricio.svg" role="DevOps"></Item>
+        <Item
+          name="Sebastián Soto"
+          src="sebastian.svg"
+          role="Scrum Master"
+        ></Item>
+      </Grid>
+    </Container>
+  );
+}
+
+export default Integrantes;
